354fe3e30fb26e4df4993b3e6c415e851d065b38
MMernitz <marcelmernitz@googlemail.com>
2023-09-22 15:29:27 +0200

    Update Example.js
    Filters are now parseable

--- src/Example.js
+++ src/Example.js
@@ -1,57 +1,114 @@
-import { NavLink, useSearchParams } from "react-router-dom";
+import { Link, Outlet, useSearchParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { shallow } from "zustand/shallow";
 
 import { useTestStore } from "./store";
 
 export default function Example() {
-  const [searchParams, setSearchParams] = useSearchParams({
-    q: "test",
-    sort: "canonical",
-  });
-  const { inputValue, sort, setInputValue, setSort } = useTestStore(
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const {
+    inputValue,
+    sort,
+    filters,
+    setInputValue,
+    setSort,
+    setScribes,
+    setInstitutes,
+    setBooks,
+  } = useTestStore(
     (state) => ({
       inputValue: state.inputValue,
       sort: state.sort,
+      filters: state.filters,
       setInputValue: state.setInputValue,
       setSort: state.setSort,
+      setScribes: state.setScribes,
+      setInstitutes: state.setInstitutes,
+      setBooks: state.setBooks,
     }),
     shallow,
   );
 
-  const q = searchParams.get("q");
-  const order = searchParams.get("sort");
-
   useEffect(() => {
-    setInputValue(q);
-    setSort(order);
-  }, [q, order]);
+    const q = searchParams.get("q") || "test"; // Provide a default value if "q" is missing
+    const order = searchParams.get("sort") || "canonical"; // Provide a default value if "sort" is missing
+    const facetString = decodeURIComponent(searchParams.get("filters"));
+    let facets = {};
+    try {
+      facets = JSON.parse(facetString);
+      setInputValue(q);
+      setSort(order);
+      setScribes(facets.scribes);
+      setInstitutes(facets.institutes);
+      setBooks(facets.books);
+    } catch (error) {
+      // Handle invalid JSON or missing parameters gracefully
+      console.error("Error parsing filters:", error);
+      // Provide default values or handle the error as needed
+      setInputValue("test");
+      setSort("canonical");
+      setScribes([]);
+      setInstitutes([]);
+      setBooks([]);
+    }
+  }, [
+    searchParams,
+    setInputValue,
+    setSort,
+    setScribes,
+    setInstitutes,
+    setBooks,
+  ]);
 
   function handleInputChange(event) {
     setInputValue(event.target.value);
   }
 
   function handleButtonClick() {
-    setSearchParams((prev) => prev.set("q", inputValue));
-    setSearchParams(searchParams);
+    const updatedSearchParams = new URLSearchParams();
+    updatedSearchParams.set("q", inputValue);
+    updatedSearchParams.set("sort", sort);
+    updatedSearchParams.set("filters", JSON.stringify(filters));
+
+    // Use the navigate function to update the URL
+    navigate({ search: updatedSearchParams.toString() });
   }
+
   return (
     <div>
       <div>
         <h1>Hi from Example page</h1>
         <input value={inputValue} onChange={handleInputChange} />
         {inputValue && <p>This is our input value: {inputValue}</p>}
-        {q && <p>This is our searchParam value for q: {q}</p>}
         {sort && <p>This is the sort value: {sort}</p>}
+        {filters && (
+          <div>
+            <h3>Filters</h3>
+            <div>
+              <p>All the filters: </p>
+              {/* The following mappings are iterating over the filters keys and iterate over the keys values in order to print them*/}
+              {/** p may not be a child of p */}
+              {Object.keys(filters).map((filter) => (
+                <div key={filter}>
+                  {filter}:{" "}
+                  {filters[filter].map((item) => (
+                    <p key={item}>{item}</p>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
         <button type="button" onClick={handleButtonClick}>
           Click to update the URL
         </button>
       </div>
       <button>
-        <NavLink to="/" replace={true}>
-          To the Homepage
-        </NavLink>
+        <Link to="/">To the Homepage</Link>
       </button>
+      <Outlet />
     </div>
   );
 }
