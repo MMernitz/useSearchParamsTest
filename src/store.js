35e212539970385aecd1d66fe65d2660c3f6ee0b
MMernitz <marcelmernitz@googlemail.com>
2023-09-26 16:12:37 +0200

    Update store.js
    finally working example

--- src/store.js
+++ src/store.js
@@ -3,6 +3,30 @@ import { create } from "zustand";
 export const useTestStore = create((set) => ({
   inputValue: "",
   sort: "canonical",
+  scribes: ["a", "b", "c"],
+  institutes: ["bl", "ubl"],
+  books: ["me", "ge", "de"],
+  filters: {
+    scribes: [],
+    institutes: [],
+    books: [],
+  },
+  selectedScribes: [],
+  selectedInstitutes: [],
+  selectedBooks: [],
   setInputValue: (value) => set(() => ({ inputValue: value })),
-  setSort: (value) => set(() => ({ sort: value }))
+  setSort: (value) => set(() => ({ sort: value })),
+  setScribes: (newScribes) =>
+    set((state) => ({ filters: { ...state.filters, scribes: newScribes } })),
+  setInstitutes: (newInstitutes) =>
+    set((state) => ({
+      filters: { ...state.filters, institutes: newInstitutes },
+    })),
+  setBooks: (newBooks) =>
+    set((state) => ({ filters: { ...state.filters, books: newBooks } })),
+  setSelectedScribes: (newScribes) =>
+    set(() => ({ selectedScribes: newScribes })),
+  setSelectedInstitutes: (newInsts) =>
+    set(() => ({ selectedInstitutes: newInsts })),
+  setSelectedBooks: (newBooks) => set(() => ({ selectedBooks: newBooks })),
 }));
