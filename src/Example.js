c28d858f32b5579ff772061f0c69d42eba2224e4
MMernitz <marcelmernitz@googlemail.com>
2023-09-29 16:54:59 +0200

    Update Example.js

--- src/Example.js
+++ src/Example.js
@@ -6,6 +6,7 @@ import { useTestStore } from "./store";
 
 export default function Example() {
   const [buttonClicked, setButtonClicked] = useState(false);
+  const [serverResponse, setServerResponse] = useState();
   const [searchParams, setSearchParams] = useSearchParams({
     q: "test",
     sort: "canonical",
@@ -18,6 +19,7 @@ export default function Example() {
     institutes,
     books,
     filters,
+    loading,
     selectedScribes,
     selectedInstitutes,
     selectedBooks,
@@ -29,6 +31,10 @@ export default function Example() {
     setSelectedScribes,
     setSelectedInstitutes,
     setSelectedBooks,
+    setLoading,
+    setScribesData,
+    setInstitutesData,
+    setBooksData,
   } = useTestStore(
     (state) => ({
       inputValue: state.inputValue,
@@ -37,6 +43,7 @@ export default function Example() {
       institutes: state.institutes,
       books: state.books,
       filters: state.filters,
+      loading: state.loading,
       selectedScribes: state.selectedScribes,
       selectedInstitutes: state.selectedInstitutes,
       selectedBooks: state.selectedBooks,
@@ -48,14 +55,34 @@ export default function Example() {
       setSelectedScribes: state.setSelectedScribes,
       setSelectedInstitutes: state.setSelectedInstitutes,
       setSelectedBooks: state.setSelectedBooks,
+      setLoading: state.setLoading,
+      setScribesData: state.setScribesData,
+      setInstitutesData: state.setInstitutesData,
+      setBooksData: state.setBooksData,
     }),
-    shallow,
+    shallow
   );
+
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => setServerResponse(data));
+  }, []);
+
+  useEffect(() => {
+    if (serverResponse) {
+      setScribesData(serverResponse.data.scribes);
+      setInstitutesData(serverResponse.data.institutes);
+      setBooksData(serverResponse.data.books);
+      setLoading(false);
+    }
+  }, [serverResponse]);
 
   useEffect(() => {
     const q = searchParams.get("q"); // Provide a default value if "q" is missing
     const order = searchParams.get("sort"); // Provide a default value if "sort" is missing
     const facetString = decodeURIComponent(searchParams.get("filters"));
+    console.log("facetString: ", facetString);
     let facets = {};
     try {
       facets = JSON.parse(facetString);
@@ -69,7 +96,7 @@ export default function Example() {
       setSelectedScribes(facets.scribes);
     } catch (error) {
       // Handle invalid JSON or missing parameters gracefully
-      console.error("Error parsing filters:", error);
+      console.log("Error parsing filters:", error);
       // Provide default values or handle the error as needed
       setInputValue("test");
       setSort("canonical");
@@ -103,6 +130,7 @@ export default function Example() {
       setButtonClicked(false);
     }
   }, [buttonClicked]);
+
   function handleInputChange(event) {
     setInputValue(event.target.value);
   }
@@ -142,8 +170,12 @@ export default function Example() {
         <h1>Hi from Example page</h1>
         <input value={inputValue} onChange={handleInputChange} />
         {inputValue && <p>This is our input value: {inputValue}</p>}
+        {searchParams.get("q") && (
+          <p>This is the input from the URL: {searchParams.get("q")} </p>
+        )}
         {sort && <p>This is the sort value: {sort}</p>}
-        {filters && (
+
+        {!loading && filters && (
           <div>
             <h3>Filters</h3>
             <div>
